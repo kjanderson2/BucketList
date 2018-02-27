e2c7dd39955d11e44b97b9b25f812edd82d7b06e
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2018-02-27 11:06:24 -0800

    updated ListItem data class for nullity and removed faulty update function

--- app/src/main/java/android/kaitlynanderson/com/bucketlist/model/ListItem.kt
+++ app/src/main/java/android/kaitlynanderson/com/bucketlist/model/ListItem.kt
@@ -4,7 +4,10 @@ import java.util.*
 
 /**
  * Created by kaitlynanderson on 2/27/18.
+ * Data class for a list item
+ * Required non-null parameter of title
+ * All other parameters are optional and add information about the users goals/priorities
  */
-data class ListItem(var title: String, var description: String, var cost: Double, var priority: Int, var deadline: Date, var notes: String) {
-    fun updateTitle()
+data class ListItem(var title: String, var description: String?, var cost: Double?, var priority: Int?, var deadline: Date?, var notes: String?) {
+
 }
